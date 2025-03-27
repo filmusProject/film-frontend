72f36863014c61b3909cf35ca057f99166592e57
taco-recipe <taco9590@gmail.com>
2025-03-27 20:51:42 +0900

    feat: 페이지 라우팅 수정

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import HomePage from './pages/HomePage';
-import SearchPage from './pages/SearchPage';
-import MovieDetailPage from './pages/MovieDetailPage';
-import RecommendPage from './pages/RecommendPage';
-import TogetherPage from './pages/TogetherPage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import Mypage from './pages/Mypage';
-import FindUsernamePage from './pages/FindUsernamePage';
-import ResetPasswordPage from './pages/ResetPasswordPage';
-import ChangePasswordPage from './pages/ChangePasswordPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/movie-detail" element={<MovieDetailPage />} />
-        <Route path="/recommend" element={<RecommendPage />} />
-        <Route path="/together" element={<TogetherPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/mypage" element={<Mypage />} />
-        <Route path="/find-username" element={<FindUsernamePage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
-        <Route path="/change-password" element={<ChangePasswordPage />} />
-      </Routes>
-    </Router>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/movie-detail" element={<MovieDetailPage />} />
+          <Route path="/recommend" element={<RecommendPage />} />
+          <Route path="/together" element={<TogetherPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/find-username" element={<FindUsernamePage />} />
+          <Route path="/reset-password" element={<ResetPasswordPage />} />
+          <Route path="/change-password" element={<ChangePasswordPage />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
-
-export default App;
